1e0fba9f4de7f9ea8db20e13ca725b52211c2ffc
koncic-neuralab-net <koncic@neuralab.net>
2013-01-13 17:44:32 +0100

    Number with dots generation

--- MathAndNumbersUtils.cs
+++ MathAndNumbersUtils.cs
@@ -7,13 +7,56 @@ namespace Net_Neuralab_Utilities
 {
     class MathAndNumbersUtils
     {
-        //This method is used when returning decimal numbers directly from database. MSSQL DB actually returns string representation so hence the parsing to decimal insdide this method///////////////////////////
-
         public static string roundedTwoDecimalPlacesString(string decNum)
         {
+            //This method is used when returning decimal numbers directly from database. MSSQL DB actually returns string representation so hence the parsing to decimal insdide this method///////////////////////////
+
             decimal dn = decimal.Parse(decNum);
 
             return Math.Round(dn, 2).ToString();
+        }
+
+
+        public static string generateNumStringWithDots(string numWithoutDots)
+        {
+            //The method accepts string numbers with comma as a decimal delimiter
+
+            string returnString = "";
+
+            string[] parts = numWithoutDots.Split(',');
+
+            char[] firstPart = parts[0].ToCharArray();
+
+            int lastPos = firstPart.Length - 1;
+
+            int counter = 1;
+
+            while (lastPos > -1)
+            {
+                if ((counter % 3 == 0) && (lastPos > 0))
+                {
+                    returnString = returnString.Insert(0, "." + firstPart[lastPos].ToString());
+                }
+                else
+                {
+                    returnString = returnString.Insert(0, firstPart[lastPos].ToString());
+                }
+
+                lastPos--;
+                counter++;
+            }
+
+
+            if (parts.Length == 1)
+            {
+                returnString = returnString + ",00";
+            }
+            else
+            {
+                returnString = returnString + "," + parts[1];
+            }
+
+            return returnString;
         }
     }
 }
